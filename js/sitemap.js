7a35532a028765b940c2adbbc16fb87b6b3f8b8c
junyeong94 <64447875+junyeong94@users.noreply.github.com>
2020-04-29 17:57:16 +0900

    Add files via upload

---
+++ js/sitemap.js
@@ -0,0 +1,11 @@
+$(function(){
+$('#sitemap_btn').click(function(){
+    $('.sitemap').toggle();
+    var cnt=$('#sitemap_btn');
+    if(cnt.attr('src')=='images/hamburger01.png'){
+        cnt.attr('src','images/hamburger02.png');
+    }else{
+        cnt.attr('src','images/hamburger01.png');
+    }
+});
+});
